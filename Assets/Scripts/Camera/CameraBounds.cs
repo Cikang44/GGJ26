b3aebe0ae9df9af8588204243bcbe124662aae33
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-02-08 02:15:38 +0700

    fix: Camera Bounds has wrong center

--- Assets/Scripts/Camera/CameraBounds.cs
+++ Assets/Scripts/Camera/CameraBounds.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CameraBounds : MonoBehaviour
 {
+    public Vector2 boundsCenter = new(0, 0);
     public Vector2 boundsSize = new(15, 15);
     private Camera _camera;
     private Bounds _bounds;
@@ -10,7 +11,7 @@ public class CameraBounds : MonoBehaviour
     void Start()
     {
         _camera = GetComponent<Camera>();
-        _bounds = new Bounds(transform.position, boundsSize);
+        _bounds = new Bounds(boundsCenter, boundsSize);
     }
 
     // Update is called once per frame
@@ -18,9 +19,9 @@ public class CameraBounds : MonoBehaviour
     {
         boundsSize.x = Mathf.Max(boundsSize.x, _camera.orthographicSize * _camera.aspect * 2);
         boundsSize.y = Mathf.Max(boundsSize.y, _camera.orthographicSize * 2);
-        _bounds.max = new Vector3(boundsSize.x / 2 - _camera.orthographicSize * _camera.aspect, boundsSize.y / 2 - _camera.orthographicSize, 0);
-        _bounds.min = new Vector3(boundsSize.x / 2 - _camera.orthographicSize * _camera.aspect, boundsSize.y / 2 - _camera.orthographicSize, 0) * -1;
-        
+        _bounds.max = new Vector3(boundsCenter.x + boundsSize.x / 2 - _camera.orthographicSize * _camera.aspect, boundsCenter.y + boundsSize.y / 2 - _camera.orthographicSize, 0);
+        _bounds.min = new Vector3(boundsCenter.x + boundsSize.x / 2 - _camera.orthographicSize * _camera.aspect, boundsCenter.y + boundsSize.y / 2 - _camera.orthographicSize, 0) * -1;
+
         transform.position = new(
             Mathf.Clamp(transform.position.x, _bounds.min.x, _bounds.max.x),
             Mathf.Clamp(transform.position.y, _bounds.min.y, _bounds.max.y),
@@ -40,6 +41,6 @@ public class CameraBounds : MonoBehaviour
         // Convert the local coordinate values into world
         // coordinates for the matrix transformation.
         Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.DrawWireCube(Vector3.zero, boundsSize);
+        Gizmos.DrawWireCube(boundsCenter, boundsSize);
     }
 }
