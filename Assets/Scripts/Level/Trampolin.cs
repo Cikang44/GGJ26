92ca1224283c1f4a8e8a2340867401f91df2aa89
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-02-07 17:04:25 +0700

    feat: Add comically destroyed trampoline when get jumped

---
+++ Assets/Scripts/Level/Trampolin.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Trampolin : MonoBehaviour
+{
+    public bool canBeThrowed = true;
+
+    private Rigidbody2D _rigidbody;
+    private Collider2D[] _colliders;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _colliders = GetComponents<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            foreach (var collider in _colliders)
+            {
+                collider.enabled = false;
+            }
+            _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+            float angle = Random.Range(60, 120) * Mathf.Deg2Rad;
+            _rigidbody.AddForce(new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * 500);
+            _rigidbody.AddTorque(Random.Range(-720, 720) * Mathf.Deg2Rad);
+        }
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -100)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
