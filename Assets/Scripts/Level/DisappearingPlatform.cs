3e51f4041171e5a38537479569ea59fdcc9ccb9f
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-02-07 16:26:36 +0700

    feat: Fade animation for disappearing platform

--- Assets/Scripts/Level/DisappearingPlatform.cs
+++ Assets/Scripts/Level/DisappearingPlatform.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class DisappearingPlatform : MonoBehaviour
 {
-    private Renderer _renderer;
+    private SpriteRenderer _renderer;
     private Collider2D _collider;
 
     public bool canAppearAgain = true;
@@ -12,7 +13,7 @@ public class DisappearingPlatform : MonoBehaviour
 
     void Start()
     {
-        _renderer = GetComponent<Renderer>();
+        _renderer = GetComponent<SpriteRenderer>();
         _collider = GetComponent<Collider2D>();
     }
 
@@ -23,9 +24,26 @@ public class DisappearingPlatform : MonoBehaviour
 
     private IEnumerator DisappearCoroutine()
     {
-        yield return new WaitForSeconds(disappearDelay);
-        if (_renderer != null) _renderer.enabled = false;
+        float timePassed = 0f;
+        while (timePassed < disappearDelay)
+        {
+            yield return null;
+            timePassed += Time.deltaTime;
+            Color color = _renderer.color;
+            color.a = Mathf.Cos(timePassed * Mathf.PI * 2) / 3f + 0.6667f;
+            _renderer.color = color;
+        }
         if (_collider != null) _collider.enabled = false;
+        timePassed = 0f;
+        while (timePassed < 0.25f)
+        {
+            yield return null;
+            timePassed += Time.deltaTime;
+            Color color = _renderer.color;
+            color.a = (0.25f - timePassed) / 0.25f;
+            _renderer.color = color;
+        }
+        if (_renderer != null) _renderer.enabled = false;
         if (canAppearAgain) Appear();
     }
 
@@ -38,6 +56,15 @@ public class DisappearingPlatform : MonoBehaviour
     {
         yield return new WaitForSeconds(appearDelay);
         if (_renderer != null) _renderer.enabled = true;
+        float timePassed = 0f;
+        while (timePassed < 0.5f)
+        {
+            yield return null;
+            timePassed += Time.deltaTime;
+            Color color = _renderer.color;
+            color.a = timePassed / 0.5f;
+            _renderer.color = color;
+        }
         if (_collider != null) _collider.enabled = true;
     }
 }
